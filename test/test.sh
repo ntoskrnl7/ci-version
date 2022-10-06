7a875ec089541a17b4c4598b8b0e5380d0e025a8
Jung-Kwnag Lee <jungkwang.lee@softcamp.co.kr>
2022-10-06 13:23:48 +0900

    Fix test script

--- test/test.sh
+++ test/test.sh
@@ -38,48 +38,45 @@ function build_and_test() {
         res=`./build/Debug/test`
     fi
 
-    res2=`$bin/semver.sh`
-    
-    if [ "$res" == "$res2"  ]; then
-        echo [OK]$res == $res2
+    if [ "$1" == "$res"  ]; then
+        echo [OK]$res
     else
-        echo  [FAILED] $res != $res2
+        echo  [FAILED] excepted: $1, actual: $res
         ret=`expr $ret + 1`
     fi
 }
 
-build_and_test
+build_and_test '0.0.0-alpha'
 
 $bin/major.sh 2
-build_and_test
+build_and_test '2.0.0-alpha'
 
 $bin/release.sh
-build_and_test
+build_and_test '2.0.0'
 
 $bin/build-metadata.sh ci-version.test
-build_and_test
+build_and_test '2.0.0+ci-version.test'
 
 $bin/pre-release.sh rc.2
-build_and_test
+build_and_test '2.0.0-rc.2+ci-version.test'
 
 $bin/patch.sh
-build_and_test
+build_and_test '2.0.1-rc.2+ci-version.test'
 
 $bin/build-metadata.sh 
 $bin/major.sh
-build_and_test
+build_and_test '3.0.0-rc.2'
 
 $bin/pre-release.sh rtm
 $bin/minor.sh
-build_and_test
+build_and_test '3.1.0-rtm'
 
 $bin/major.sh
 $bin/build-metadata.sh `date '+%y%m%d'`
 $bin/release.sh
-build_and_test
+build_and_test '4.0.0+'`date '+%y%m%d'`
 
 rm -rf .config
-
 rm -rf build
 
 exit $ret
